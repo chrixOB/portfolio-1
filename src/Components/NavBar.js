1f5778ca4f5e2b07d7ed9b2a7214b7133cf7de96
totimehjames123 <totimehjames123@gmail.com>
2024-02-17 13:29:32 +0000

    navbar commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from "next/link";
+import { useRouter } from 'next/router';
+
+function NavBar() {
+
+    const router = useRouter();
+  
+    const navLinks = [
+        {
+            name: 'Home',
+            link: '',
+        },
+        {
+            name: 'About',
+            link: '',
+        },
+        {
+            name: 'Services',
+            link: '',
+        },
+        {
+            name: 'Projects',
+            link: '',
+        },
+        {
+            name: 'Contacts',
+            link: '',
+        }
+    ]
+
+  return (
+    <div className={'flex justify-between p-5 overflow-hidden'}>
+        <div>
+          <Link href={'#'} className="text-[20px]">Portfolio</Link>
+        </div>
+        <ul className="flex">
+         {navLinks.map((navLink, index) => 
+                
+            <li key={index} className="mr-5"><Link href={`#${navLink.name}`} className={`${router.pathname.includes(navLink.name) == true ? 'text-blue-200' : ''}`}>{navLink.name}</Link></li>
+        )}
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import NavBar from "@/Components/NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      Hello from next.js
+    <style jsx>{`
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+        * {
+          font-family: Poppins, sans-serif;
+        }
+      `}</style>
+      
+      <NavBar />
+      
     </div>
   )
 }
