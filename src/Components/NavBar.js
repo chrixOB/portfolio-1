ec2c0a759577f1a3d2ee2731b2928f0fd2f87562
totimehjames123 <totimehjames123@gmail.com>
2024-02-17 14:05:52 +0000

    .vercel removed from .gitignore

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -30,9 +30,9 @@ function NavBar() {
     ]
 
   return (
-    <div className={'flex justify-between p-5 overflow-hidden border'}>
+    <div className={'flex justify-between p-5 overflow-hidden '}>
         <div>
-          <Link href={'#'} className="text-[20px] text-green-500">Portfolios</Link>
+          <Link href={'#'} className="text-[20px]">Portfolios</Link>
         </div>
         <ul className="flex">
          {navLinks.map((navLink, index) => 
