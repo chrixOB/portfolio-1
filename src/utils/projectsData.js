d9c4e33225b1b33f1bb7fe9dcc514e1d3ff59791
totimehjames123 <totimehjames123@gmail.com>
2024-07-25 15:42:36 +0000

    add project 2

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -1,15 +1,9 @@
 
 const projectsData = [
     {
-        title: "Pharmacy Inventory System",
-        description: "Built with ReactJS, MongoDB, NodeJS - Express",
-        link: "https://pharmacy-inventory-system.vercel.app",
-        type: "Web Application"
-    },
-    {
-        title: "Job Application System",
-        description: "Apply for Jobs, Internships & Course online. Built with HTML, CSS, Booststrap, JavaScript, PHP & SQL",
-        link: "https://emit-tommel.000webhostapp.com",
+        title: "Event management System",
+        description: "Manage and notify ATU students about upcoming events.",
+        link: "https://atu-event-management-system.vercel.app/",
         type: "Web Application"
     },
     {
@@ -19,15 +13,20 @@ const projectsData = [
         type: "Web Application"
     },
     {
-        title: "Event management System",
-        description: "Manage and notify ATU students about upcoming events.",
-        link: "https://atu-event-management-system.vercel.app/",
-        type: "Web Application"
-    },
-    {
         title: "Chat Application",
         description: "Simple Chat Application built with MERN",
         link: "https://chat-app-with-mern.vercel.app/",
+        type: "Web Application"
+    },{
+        title: "Pharmacy Inventory System",
+        description: "Built with ReactJS, MongoDB, NodeJS - Express",
+        link: "https://pharmacy-inventory-system.vercel.app",
+        type: "Web Application"
+    },
+    {
+        title: "Job Application System",
+        description: "Apply for Jobs, Internships & Course online. Built with HTML, CSS, Booststrap, JavaScript, PHP & SQL",
+        link: "https://emit-tommel.000webhostapp.com",
         type: "Web Application"
     },
     
