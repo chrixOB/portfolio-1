ed6a633d9ea5e85858d1d2b5f5b9821694ecb416
totimehjames123 <totimehjames123@gmail.com>
2024-07-25 21:44:26 +0000

    color button at disabled

--- src/components/ButtonBackground.js
+++ src/components/ButtonBackground.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ButtonBackground({title, onPress, disabled}) {
   return (
     <>
-        <button disabled={disabled} className={`hover:bg-transparent  ${disabled ? 'border-2 hover:bg-none border-blue-400 text-cyan-500' : 'bg-cyan-300 hover:text-cyan-300 border-2 text-black border-cyan-300 shadow-cyan-300  shadow-sm'} rounded-lg mr-3 p-2`}>{title}</button>
+        <button disabled={disabled} className={`hover:bg-transparent  ${disabled ? 'border-2 hover:bg-none border-opacity-70 text-cyan-500' : 'bg-cyan-300 hover:text-cyan-300 border-2 text-black border-cyan-300 shadow-cyan-300  shadow-sm'} rounded-lg mr-3 p-2`}>{title}</button>
     </>
   )
 }
